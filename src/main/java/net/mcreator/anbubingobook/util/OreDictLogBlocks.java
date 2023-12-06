4a86d2499e1701e677d740f5390bbb7b49cc47e3
bobzeus <bobzeus@email.com>
2023-12-06 13:13:44 +0000

    transfer
    transfer from laptop to pc, will fix jei, add info and hopefully either add recipie or advancement soon

---
+++ src/main/java/net/mcreator/anbubingobook/util/OreDictLogBlocks.java
@@ -0,0 +1,17 @@
+
+package net.mcreator.anbubingobook.util;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import net.mcreator.anbubingobook.ElementsAnbubingobookMod;
+
+@ElementsAnbubingobookMod.ModElement.Tag
+public class OreDictLogBlocks extends ElementsAnbubingobookMod.ModElement {
+	public OreDictLogBlocks(ElementsAnbubingobookMod instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+}
